0030af4428024d0bd8aeb906db7ba7eea9dec270
muchilham <muh.ilham0606@gmail.com>
2018-12-28 02:28:16 +0700

    Initial commit

---
+++ lookup/DataLookUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lookup
+{
+    public partial class DataLookUp : Form
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext();
+        String primary;
+        public DataLookUp()
+        {
+            InitializeComponent();
+        }
+
+        private void DataLookUp_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.tbl_Users.ToList();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void dataGridView1_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            session.primary = this.dataGridView1[0, e.RowIndex].Value.ToString();
+            this.DialogResult = DialogResult.OK;
+        }
+    }
+}
---
+++ lookup/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lookup
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (new DataLookUp().ShowDialog() == DialogResult.OK)
+            {
+                this.textBox1.Text = session.primary;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
